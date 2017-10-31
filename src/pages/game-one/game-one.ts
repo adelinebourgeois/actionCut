d19332c233080b2d65b7c7e62f4c03c1a1ffa631
anaiskhaldi <anaiskhaldi@MacBook-Pro-de-Anais.local>
2017-10-31 09:34:39 +0100

    update home et gameOne

--- src/pages/game-one/game-one.ts
+++ src/pages/game-one/game-one.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController} from 'ionic-angular';
+import { NavController, NavParams} from 'ionic-angular';
+import {SQLite, SQLiteObject} from "@ionic-native/sqlite";
 
+const DATABASE_FILE_NAME: string = 'data.db';
 
 @Component({
   selector: 'page-game-one',
@@ -8,8 +10,17 @@ import { NavController} from 'ionic-angular';
 })
 export class GameOnePage {
 
-  constructor(public navCtrl: NavController) {
+  levels: string[] = [];
+
+  private db: SQLiteObject;
+
+  constructor(public navCtrl: NavController, private sqlite: SQLite, public navParams: NavParams) {
+      this.levels = navParams.get('level');
+      this.retrieveQuestion();
   }
 
+  public retrieveQuestion() {
+    console.log('this is the question : ' + this.levels);
+  }
 
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { NavController} from 'ionic-angular';
+import { GameOnePage } from "../game-one/game-one";
 import {SQLite, SQLiteObject} from "@ionic-native/sqlite";
 
 const DATABASE_FILE_NAME: string = 'data.db';
@@ -12,11 +13,12 @@ export class HomePage {
 
     private db: SQLiteObject;
 
-    game: string[] = [];
+    levels: string[] = [];
 
-    constructor(private sqlite: SQLite) {
+    constructor(public navCtrl: NavController, private sqlite: SQLite) {
         this.createDbFile();
     }
+
 
     private createDbFile(): void {
         this.sqlite.create({
@@ -26,9 +28,10 @@ export class HomePage {
         .then((db: SQLiteObject) => {
             console.log('Create BDD !');
             this.db = db;
+            this.deleteContent();
             this.createTables();
             this.insertDb();
-
+            this.retrieveDataGameOne();
         })
         .catch(e => console.log(e));
     }
@@ -53,26 +56,53 @@ export class HomePage {
     private insertDb(): void {
         this.db.executeSql('INSERT INTO `Niveaux` (status, gameType, question, info)VALUES(1, 1, "Quel réalisateur aurait pu réaliser James Bond ?", "Steven Spielberg, qui venait de connaître un immense succès avec Les dents de la mer, voulut réaliser un James Bond, ce qui lui fut refusé par les producteurs. Il décida alors de passer à autre chose sur les conseils de Georges Lucas, et c\'est ainsi que naquit la série Indiana Jones.")', {})
             .then(() => console.log('Insert niveau 1 created'))
-            .catch(e => console.log(e))
+            .catch(e => console.log(e));
+
+        this.db.executeSql('INSERT INTO `Niveaux` (status, gameType, question, info)VALUES(1, 1, "Test ?", "This is a test.")', {})
+            .then(() => console.log('Insert niveau 2 created'))
+            .catch(e => console.log(e));
+
+
     }
 
-    public retriewDataGameOne() {
-      this.game= [];
-      this.db.executeSql('SELECT * FROM `Niveaux` CROSS JOIN `Reponse` WHERE gameType = 1', {})
+    public retrieveDataGameOne() {
+
+      // 'SELECT * FROM `Niveaux` CROSS JOIN `Reponses` WHERE gameType = 1'
+      this.db.executeSql('SELECT question FROM `Niveaux`', {})
           .then((data) => {
             if(data == null) {
+              // console.log('data is null');
               return;
             }
-            if(data.row) {
-              if(data.row.length > 0) {
-                for(let i = 0; i < data.row.length; i++) {
-                  console.log(data.row.item(i));
-                  //this.game.push(data.row.item(i).id)
+            if(data.rows) {
+              if(data.rows.length > 0) {
+                for(let i = 0; i < data.rows.length; i++) {
+                  console.log('Mes datas : ' + JSON.stringify(data.rows.item(i)));
+                  this.levels.push(data.rows.item(i).question);
                 }
               }
             }
           })
           .catch( e => console.log(e))
     }
+
+    public displayInfo(){
+      console.log(this.levels);
+    }
+
+    private deleteContent(): void {
+        this.db.executeSql('DELETE from `Niveaux`', {})
+            .then(() => console.log('Delete content'))
+            .catch(e => console.log(e))
+    }
+
+    private showGame(theLevel){
+      this.navCtrl.push(GameOnePage, {
+          level: theLevel,
+      });
+    }
+
+
+
 
 }
